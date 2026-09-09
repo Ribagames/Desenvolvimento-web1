160c2307b329e2945f39122f5f46c68dd64ee8b6
Ribagames <joaquim78if@gmail.com>
2026-09-09 12:36:21 +0000

    vai ter atividade

--- login.js
+++ login.js
@@ -11,3 +11,9 @@ avatarImg.src =
 "https://cdn-icons-png.flaticon.com/512/3135/3135715.png";
 avatarImg.alt = "Avatar do Usuário";
 
+const title = document.createElement(h1);
+title.className = "login-title";
+title.texteContent = "Área secreta";
+
+const userInput = document.createElement("input");
+userInput.type = "text;"
