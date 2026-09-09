72c9381bee87e608d671a0f834e6a7c6d3b671ab
Ribagames <joaquim78if@gmail.com>
2026-09-09 12:03:03 +0000

    login

---
+++ login.js
@@ -0,0 +1,13 @@
+const canvas = document.createElement("canvas");
+canvas.id = "canvas";
+document.body.appendChild(canvas);
+
+const loginCard = document.createElement("div");
+loginCard.className = "login-Card";
+
+const avatarImg = document.createElement("img");
+avatarImg.className = "login-avatar";
+avatarImg.src =
+"https://cdn-icons-png.flaticon.com/512/3135/3135715.png";
+avatarImg.alt = "Avatar do Usuário";
+
