998d144a6086c009da9285dee134d814f5b29b50
Ribagames <joaquim78if@gmail.com>
2026-09-09 13:44:47 +0000

    ir embora

--- login.js
+++ login.js
@@ -17,3 +17,32 @@ title.texteContent = "Área secreta";
 
 const userInput = document.createElement("input");
 userInput.type = "text;"
+userInput.placeholder = "Usuário / E-mail"
+userInput.className = "Input-field"
+
+const passInput = document.createElement("Button");
+passInput.type = "password";
+passInput.placeholder = "Sua senha";
+passInput.className = "input-field";
+
+const loginBtn = document.createElement = ("Button");
+loginBtn.className = "btn-submit";
+loginBtn.texteContent ="entrar";
+loginBtn.addEventListener("click", () => {
+    if (userInput.value && passInput.value) {
+   alert(`Bem-vindo, ${userInput.value}! Login realizado com sucesso.`);
+ } else {
+   alert("Por favor, preencha todos os campos!");
+ }
+});
+
+
+
+loginCard.appendChild(avatarImg);
+loginCard.appendChild(title);
+loginCard.appendChild(userInput);
+loginCard.appendChild(passInput);
+loginCard.appendChild(loginBtn);
+
+document.body.appendChild(loginCard);
+
